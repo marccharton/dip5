ffb49095b71a521e101765e06c52d32a8c3a2452
narcomarch <kramoogle@gmail.com>
2018-09-01 13:16:13 +0200

    feat(01): change some things to test node server installation

--- 01/js/sketch.js
+++ 01/js/sketch.js
@@ -1,12 +1,21 @@
+
 function setup() {
     createCanvas(1080, 720);
+
+    drawingContext.shadowOffsetX = 5;
+  	drawingContext.shadowOffsetY = -5;
+  	drawingContext.shadowBlur = 50;
+  	drawingContext.shadowColor = "black";
+  	background(200);
 }
 
 function draw() {
     if (mouseIsPressed) {
         fill(0);
         stroke(255);
+        drawingContext.shadowColor = "blue";
     } else {
+    	drawingContext.shadowColor = "black";
         fill(255);
         stroke(0);
     }
